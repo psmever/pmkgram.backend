599946d3f0c324df219fd9a4c57a3246d40df52c
psmever <psmever@gmail.com>
2023-03-25 16:28:45 +0900

    build: commitlint 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-const sampleValue = 100
+interface SampleType {
+    sample : string
+    value: number
+}
+
+const sampleValue: SampleType = {
+    sample: 'test',
+    value: 0
+}
 
 console.debug('Hello World', sampleValue)
