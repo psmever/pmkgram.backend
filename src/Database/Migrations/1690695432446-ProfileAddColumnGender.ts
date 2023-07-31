b00d48e4b678f0c0496a38f8f3f258c2272c560b
wem519 <wem519@gmail.com>
2023-07-31 10:46:57 +0900

    build: 기본 프로필 성별 추가

--- src/Database/Migrations/1690695432446-ProfileAddColumnGender.ts
+++ src/Database/Migrations/1690695432446-ProfileAddColumnGender.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm'
 
 export class ProfileAddColumnGender1690695432446 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE profile ADD gender char(6) null AFTER profile_image_id`)
+        await queryRunner.query(`ALTER TABLE profile ADD gender char(6) default '050010' AFTER profile_image_id`)
 
         await queryRunner.createForeignKey(
             'profile',
