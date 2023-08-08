dd52e506c6e366a56b0c764cc08d67259d3d42ae
psm <psm@healthmax.co.kr>
2023-08-08 11:56:25 +0900

    build: feed list 페이징 처리

--- src/Route/Api.ts
+++ src/Route/Api.ts
@@ -5,7 +5,7 @@ import { Register, EmailExits, Login, Logout, TokenRefresh, TokenInfo } from '@C
 import { RestAuthenticateMiddleware } from '@Middlewares/RestAuthenticateMiddleware'
 import { ImageCreate } from '@Controllers/Api/MediaController'
 import { ProfileEdit, NickNameExits, MyProfile } from '@Controllers/Api/MemberController'
-import { DeleteFeed, SaveFeed, UpdateFeed, MainList, FixGreat, PersonalList } from '@Controllers/Api/FeedController'
+import { DeleteFeed, SaveFeed, UpdateFeed, MainList, FixGreat, MyPersonalList } from '@Controllers/Api/FeedController'
 
 export const TestsRouter = Router()
 export const SystemRouter = Router()
@@ -48,6 +48,6 @@ MemberRouter.post('/profile-update', RestAuthenticateMiddleware, ProfileEdit)
 FeedRouter.post('/feed-save', RestAuthenticateMiddleware, SaveFeed)
 FeedRouter.put('/feed-update', RestAuthenticateMiddleware, UpdateFeed)
 FeedRouter.delete(`/:feed([0-9]+)/feed-delete`, RestAuthenticateMiddleware, DeleteFeed)
-FeedRouter.get(`/feed-list`, MainList)
-FeedRouter.get(`/my-feed-list`, RestAuthenticateMiddleware, PersonalList)
+FeedRouter.get(`/:lastId([0-9]+)/feed-list`, MainList)
+FeedRouter.get(`/my-feed-list`, RestAuthenticateMiddleware, MyPersonalList)
 FeedRouter.put(`/:feed([0-9]+)/great`, RestAuthenticateMiddleware, FixGreat)
