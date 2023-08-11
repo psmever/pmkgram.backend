611e900520314d4c1abb6950c1fa319d153c01ab
Jin-composition <jinhy921207@naver.com>
2023-08-11 13:21:12 +0900

    feat: 닉네임으로 개인프로필 가져오는 api 추가

--- src/Controller/Api/MemberController.ts
+++ src/Controller/Api/MemberController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { ClientErrorResponse, SuccessDefault, SuccessResponse } from '@Commons/ResponseProvider'
 import _ from 'lodash'
 import { Logger } from '@Logger'
-import { nickNameExits, updateNickName, getUserProfile } from '@Service/UserService'
+import { nickNameExits, updateNickName, getUserProfile, getUserProfileByNickname } from '@Service/UserService'
 import { updateProfile, updateProfileImage } from '@Service/ProfileService'
 import { mediaExits } from '@Service/MediaService'
 import Messages from '@Messages'
@@ -13,6 +13,36 @@ export const MyProfile = async (req: Request, res: Response): Promise<Response> 
     const userId = req.app.locals.user.user_id
 
     const infoTask = await getUserProfile({ user_id: userId })
+    if (infoTask && infoTask.profile && infoTask.profile.media) {
+        return SuccessResponse(res, {
+            email: infoTask.email,
+            nickname: infoTask.nickname,
+            profile_image: {
+                id: infoTask.profile.profile_image_id,
+                url: `${Config.MEDIA_HOSTNAME}${infoTask.profile.media.path}/${infoTask.profile.media?.filename}`,
+            },
+            gender: {
+                name: infoTask.profile.code?.name,
+                code_id: infoTask.profile.code?.code_id,
+            },
+            intro: infoTask.profile.intro,
+        })
+    } else {
+        return ClientErrorResponse(res)
+    }
+}
+
+// 닉네임으로 내 프로필
+export const MyProfileByNickname = async (req: Request, res: Response): Promise<Response> => {
+    const { nickname } = req.params
+    const getUserId = await getUserProfileByNickname(nickname)
+
+    let infoTask;
+
+    if(getUserId){
+      infoTask = await getUserProfile({ user_id: getUserId?.id })
+    }
+
     if (infoTask && infoTask.profile && infoTask.profile.media) {
         return SuccessResponse(res, {
             email: infoTask.email,
